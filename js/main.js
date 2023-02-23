669330f26ef5688993885c1e610f482b5bf1baeb
Herenyon <rikitur17@gmail.com>
2023-02-23 21:45:17 +0100

    aggiunta lista html con lo stesso comportamento della console non c'è css

--- js/main.js
+++ js/main.js
@@ -1,23 +1,41 @@
 'use strict';
-
-for (let i = 1; i <= 100; i++){
-
-    if (i % 3 === 0 && i % 5 === 0){
-
+// const ul per prendere la classe list dall'ul nell'html 
+const ul = document.querySelector("ul.list")
+// variabile index non chiamata per dargli il valore all'interno del for
+let index
+// for che fino a 100 continua a loopare
+for (let i = 1; i <= 100; i++) {
+    // if per poter stampare fizzbuzz soltanto al posto dei numeri sia divisibili per 3 che per 5 
+    if (i % 3 === 0 && i % 5 === 0) {
+        
+        index = "FizzBuzz";
         console.log('FizzBuzz');
+        const element = `<li class="box box--${index}">${index}</li>`;
+        ul.innerHTML += element;
     }
-
+    // else if per poter stampare buzz solo al posto dei numeri divisibili per 5
     else if (i % 5 === 0) {
+        
+        const element = `<li class="box box--${index}">${index}</li>`;
+        ul.innerHTML += element;
+        index = "Buzz";
         console.log('Buzz');
     }
+    // else if per poter stampare buzz solo al posto dei numeri divisibili per 3
+    else if (i % 3 === 0) {
 
-    else if (i % 3 === 0){
-
+        index = "Fizz";
         console.log('Fizz');
+        const element = `<li class="box box--${index}">${index}</li>`;
+        ul.innerHTML += element;
     }
+    // else per stampare tutti gli altri numeri che non vengno compresi dalle condizioni precedenti
+    else {
 
-    else{
         console.log(i);
+        const element = `<li class="box box--${i}">${i}</li>`;
+        ul.innerHTML += element;
 
     }
+
 }
